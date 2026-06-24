b68f4e944a562d06a031b8dcce0a18dfffda2a7e
CarlosUngi <carlos.ungi@gmail.com>
2026-06-23 19:19:18 -0500

    Pagina inicial con Hola mundo

---
+++ src/app/presentation/tasks/tasks.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-tasks',
+    templateUrl: './tasks.page.html',
+    styleUrls: ['./tasks.page.scss'],
+    standalone: false
+})
+export class TasksPage implements OnInit {
+    title: string = 'Accenture To-Do List';
+
+    constructor() { }
+
+    ngOnInit() {
+        console.log('¡Hola Mundo!');
+    }
+}
